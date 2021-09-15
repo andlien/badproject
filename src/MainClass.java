76c8072716ab03892012e7c446df03a5e08f1067
andlie <anders.lien@entur.org>
2021-09-15 13:01:26 +0200

    Add some more trains

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,16 +1,46 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainClass {
     public static void main(String[] args) {
-        Train tog = new Train(100, 1, "Harald Haraldsen");
-        tog.stops = Arrays.asList("Oslo S", "Lillestrøm", "Oslo Lufthavn", "Eidsvoll", "Lillehammer", "Oppdal", "Trondheim");
-        tog.leggTilVogn(new Vogn(1, "B5", 100));
-        tog.leggTilVogn(new Vogn(2, "B7", -100));
-        tog.leggTilVogn(new Vogn(3, "B7", 20));
+        List<Train> togAvganger = new ArrayList<>();
 
-        System.out.print("Train is driven by " + tog.locomotiveDriverName);
-        System.out.println(" and stops at " + tog.stops.size() + " stops.");
-        System.out.println("The capacity of the train is " + countPassengers(tog));
+        Train togTilTrondheim = new Train(100, 1, "Harald Haraldsen");
+        togTilTrondheim.stops = Arrays.asList("Oslo S", "Lillestrøm", "Oslo Lufthavn", "Eidsvoll", "Lillehammer", "Oppdal", "Trondheim");
+        togTilTrondheim.leggTilVogn(new Vogn(1, "B5", 100));
+        togTilTrondheim.leggTilVogn(new Vogn(2, "B7", -100));
+        togTilTrondheim.leggTilVogn(new Vogn(3, "B7", 20));
+
+        togAvganger.add(togTilTrondheim);
+
+        System.out.print("Train is driven by " + togTilTrondheim.locomotiveDriverName);
+        System.out.println(" and stops at " + togTilTrondheim.stops.size() + " stops.");
+        System.out.println("The capacity of the train is " + countPassengers(togTilTrondheim));
+
+        Train togTilBergen = new Train(100, 2, "Anders Andersen");
+        togTilBergen.stops = Arrays.asList("Oslo S", "Sandvika", "Hønefoss", "Gol", "Voss", "Bergen");
+        togTilBergen.leggTilVogn(new Vogn(1, "B5", 100));
+        togTilBergen.leggTilVogn(new Vogn(2, "B7", -100));
+        togTilBergen.leggTilVogn(new Vogn(3, "B7", 20));
+
+        togAvganger.add(togTilBergen);
+
+        System.out.print("Train is driven by " + togTilBergen.locomotiveDriverName);
+        System.out.println(" and stops at " + togTilBergen.stops.size() + " stops.");
+        System.out.println("The capacity of the train is " + countPassengers(togTilBergen));
+
+        Train togTilStavanger = new Train(100, 3, "Hans Hansen");
+        togTilStavanger.stops = Arrays.asList("Oslo S", "Sandvika", "Kristiansand", "Egersund", "Stavanger");
+        togTilStavanger.leggTilVogn(new Vogn(1, "B5", 100));
+        togTilStavanger.leggTilVogn(new Vogn(2, "B7", -100));
+        togTilStavanger.leggTilVogn(new Vogn(3, "B7", 20));
+
+        togAvganger.add(togTilStavanger);
+
+        System.out.print("Train is driven by " + togTilStavanger.locomotiveDriverName);
+        System.out.println(" and stops at " + togTilStavanger.stops.size() + " stops.");
+        System.out.println("The capacity of the train is " + countPassengers(togTilStavanger));
     }
 
     public static int countPassengers(Train train) {
